95b1934206a00fcf1ba1b283096ae2dbfc0a7469
Daria Dubkova <rokston@mail.ru>
2023-07-04 20:43:29 +0300

    adding files

---
+++ task_3.java
@@ -0,0 +1,8 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	// Напиши код здесь
+    boolean expected = true;
+    assertEquals("Should be adult if 19", expected, isAdult);
+}
